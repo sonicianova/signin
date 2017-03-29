1c81a2562439d7829eee48b2038564b8b5070825
sonicianova <sonicianova@gmail.com>
2017-03-29 08:02:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//All programming by Andrew W. Alban
+//No data is recorded by this program, this is meant for entertainment purposes only.
+
+//Define variables:
+
+
+//Initial function:
+
+function init() {
+	
+}
